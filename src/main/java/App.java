d13a29210327f7d42e850e719ff99099d386499a
ARS <oleglyutikov1@gmail.com>
2023-08-03 15:45:32 +0300

    e

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2=(HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getMessage());
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat2.getMessage());
+        System.out.println(bean==bean2);
+        System.out.println(beanCat==beanCat2);
+
     }
 }
